1cfb1dd549f5ecf0df9d541a3ca0014e208cea9f
Rushi Sharma <rushisharma64@gmail.com>
2023-08-24 14:59:18 -0700

    Added add and delete functionalities

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import {initializeApp} from 'firebase/app';
 import{
-    getFirestore, collection, getDocs
+    getFirestore, collection, getDocs,
+    addDoc, deleteDoc, doc
 } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -35,3 +36,50 @@ getDocs(colRef)
 .catch(err => {
     console.log(err.message);
 })
+
+//add book function
+const addBookForm = document.querySelector('.add');
+
+//listen for submit event on the form
+addBookForm.addEventListener('submit', (e) => { 
+    //prevent default action which is refersing screen
+    e.preventDefault();
+
+    //store form values in variables
+    const title = addBookForm.title.value;
+    const author = addBookForm.author.value;
+
+    //add document to collection with values received from form
+    addDoc(colRef, {
+        title, 
+        author
+    })
+    .then(() => {
+    //reset form once new doc is added
+      addBookForm.reset();
+    })
+});
+
+//delete book function
+const deleteBookForm = document.querySelector('.delete');
+
+//listen for submit event on the form
+deleteBookForm.addEventListener('submit', (e) => {
+    //prevent default action which is refersing screen
+    e.preventDefault();
+
+    //store form values in variables
+    const id = deleteBookForm.id.value;
+
+    //create reference to document to be deleted
+    const docRef = doc(db, 'books', id);
+
+    //delete document from collection with values received from form
+    deleteDoc(docRef)
+    .then(() => {
+        //reset form once doc is deleted
+        deleteBookForm.reset();
+    })
+});
+
+
